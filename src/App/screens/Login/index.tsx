767ecfbaf0b42e53da812526d833e4dbc3ba19b1
aekryz1993 <ryzaek1993@gmail.com>
2022-06-08 13:31:20 +0100

    update loading check session condition

--- src/App/screens/Login/index.tsx
+++ src/App/screens/Login/index.tsx
@@ -1,9 +1,11 @@
+import { useRef } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { Status } from "../../../Enums";
 import { useAuth } from "../../../shared/providers/AuthProvider";
 import { ScreenContainer } from "../../../styles/layout/Container";
 import LoginContainer from "./components/LoginContainer";
 import Loading from "../../../App/components/Loading";
+import { getLocalStorage } from "../../../shared/hooks/useLocalStorage";
 
 interface LocationState {
   from: { pathname: string };
@@ -11,6 +13,7 @@ interface LocationState {
 
 function RedirectAuth({ children }: { children: JSX.Element }) {
   const { authState } = useAuth();
+  const tokenRef = useRef(getLocalStorage("tokenId"));
 
   const location = useLocation();
 
@@ -21,7 +24,7 @@ function RedirectAuth({ children }: { children: JSX.Element }) {
     return <Navigate to={from} replace />;
   }
 
-  if (authState.status === Status.PENDING && authState.token)
+  if (authState.status === Status.PENDING && tokenRef.current)
     return <Loading />;
 
   return children;
